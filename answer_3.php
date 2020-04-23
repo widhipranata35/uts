6fb6c942c6cadf8b6854db5eaffeb54884a57faf
widhipranata35 <widhipranata35@gmail.com>
2020-04-23 19:59:34 +0700

    answer 3

--- answer_3.php
+++ answer_3.php
@@ -1,11 +1,59 @@
 <?php
 
-$angka=array(1,2,3,4,5,6,7,8,9,10,11,12,13,15);
-$angka1=array(1,2,3,4,5,6,7,8,9,10,11,12,13,15);
-
-for($i=0; $i < count($angka); $i++){
-      $hasil[$i]=$angka[$i] * $angka1[$i];
-      echo $hasil[$i]. "</br>";
+//pembuatan fungsi
+function perkalian($angka1, $angka2)
+{
+   $a = $angka1;
+   $b = $angka2;
+   $hasil = $a * $b;
+   return $hasil;
 }
+
+//pemanggilan fungsi
+$hasil = perkalian(1, 1);
+echo " 1 * 1 = $hasil";
+echo "<br>";
+$hasil = perkalian(2, 2);
+echo " 2 * 2 = $hasil";
+echo "<br>";
+$hasil = perkalian(3, 3);
+echo " 3 * 3 = $hasil";
+echo "<br>";
+$hasil = perkalian(4, 4);
+echo " 4 * 4 = $hasil";
+echo "<br>";
+$hasil = perkalian(5, 5);
+echo " 5 * 5 = $hasil";
+echo "<br>";
+$hasil = perkalian(6, 6);
+echo " 6 * 6 = $hasil";
+echo "<br>";
+$hasil = perkalian(7, 7);
+echo " 7 * 7 = $hasil";
+echo "<br>";
+$hasil = perkalian(8, 8);
+echo " 8 * 8 = $hasil";
+echo "<br>";
+$hasil = perkalian(9, 9);
+echo " 9 * 9 = $hasil";
+echo "<br>";
+$hasil = perkalian(10, 10);
+echo " 10 * 10 = $hasil";
+echo "<br>";
+$hasil = perkalian(11, 11);
+echo " 11 * 11 = $hasil";
+echo "<br>";
+$hasil = perkalian(12, 12);
+echo " 12 * 12 = $hasil";
+echo "<br>";
+$hasil = perkalian(13, 13);
+echo " 13 * 13 = $hasil";
+echo "<br>";
+$hasil = perkalian(14, 14);
+echo " 14 * 14 = $hasil";
+echo "<br>";
+$hasil = perkalian(15, 15);
+echo " 15 * 15 = $hasil";
+echo "<br>";
 
 ?>
