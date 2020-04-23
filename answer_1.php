3c91409401bbfa74fe841c068e8693534ce6c206
widhipranata35 <widhipranata35@gmail.com>
2020-04-23 19:39:18 +0700

    answer 1

---
+++ answer_1.php
@@ -0,0 +1,20 @@
+<?php
+$angka=8;
+
+echo "nilai angka= $angka";
+echo "<br>";
+echo $angka+2;
+echo "<br>";
+echo $angka-2;
+echo "<br>";
+echo $angka+22;
+echo "<br>";
+echo $angka+2;
+echo "<br>";
+echo $angka+3;
+echo "<br>";
+echo $angka+2;
+
+
+
+?>
